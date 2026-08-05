ff0c946d63522e73c3bb92cb49754cde04a95a1f
Mohammad1724 <129998091+Mohammad1724@users.noreply.github.com>
2026-08-05 16:24:50 +0330

    Update states.py

--- app/telegram/admin/backup/states.py
+++ app/telegram/admin/backup/states.py
@@ -3,4 +3,7 @@
 BACKUP_MENU_TRIGGER = "📦 بکاپ ربات"
 BACKUP_STEP = "BackupMenu"
 SET_BACKUP_INTERVAL_STEP = "SetBackupInterval"
+RESTORE_STEP = "RestoreMenu"
+RESTORE_CONFIRM_STEP = "RestoreConfirm"
+RESTORE_WAITING_FILE_STEP = "RestoreWaitingFile"
 PANEL_STEP = "panel"
